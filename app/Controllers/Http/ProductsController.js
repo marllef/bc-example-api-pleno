2f960831f7f0b7b07b6aa5036caf57b7be4d2fdd
Marllef H. A. Freitas <marllef-alves@hotmail.com>
2022-10-11 09:33:07 -0300

    feat: add products routes

--- app/Controllers/Http/ProductsController.js
+++ app/Controllers/Http/ProductsController.js
@@ -1,11 +1,25 @@
-'use strict'
+"use strict";
+
+const Products = require("../../../assets/products.json");
 
 class ProductsController {
-  async index ({ request, response }) {
+  async index({ request, response }) {
+    response.json(Products);
   }
 
-  async show ({ params, request, response }) {
+  async show({ params, request, response }) {
+    try {
+      const product = Products.find((product) => product.id == params.id);
+
+      if (!product) throw new Error(`Produto não encontrado.`);
+
+      response.json(product);
+    } catch (err) {
+      response.status(404).json({
+        message: err.message,
+      });
+    }
   }
 }
 
-module.exports = ProductsController
+module.exports = ProductsController;
--- start/routes.js
+++ start/routes.js
@@ -1,3 +1,6 @@
 'use strict'
 
 const Route = use('Route')
+
+Route.get('/products/', 'ProductsController.index')
+Route.get('/products/:id', 'ProductsController.show')
